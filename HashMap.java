779776967988407f8570df8c6569768cad32e701
Sam Goree <samgoree@gmail.com>
2025-11-02 10:35:00 -0500

    Complete and fix bugs in HashMap.java

--- HashMap.java
+++ HashMap.java
@@ -15,69 +15,81 @@ public class HashMap<K extends Comparable<K>,V> {
     private int n;
     private int size;
 
-    public HashMap(){
-        n = 100;
+    @SuppressWarnings("unchecked")
+    public HashMap(int size){
+        n = size;
         this.data = new HashMap.Record[n];
         this.size = 0;
     }
 
+    public HashMap(){
+        this(100);
+    }
+
     private void resize(int newSize){
-        //TODO
+        Record[] oldData = this.data;
+        this.data = new HashMap.Record[newSize];
+        int hash;
+        this.n = newSize;
+        for(int i = 0; i < oldData.length; i++){
+            if(oldData[i] != null){
+                hash = oldData[i].key.hashCode();
+                hash = getIndexLinearProbing(oldData[i].key);
+                data[hash] = oldData[i];
+            }
+        }
+    }
+
+    private int fixHash(int hash){
+        return ((hash % n) + n) % n;
     }
 
     public void put(K key, V value){
-        if(this.size == n - 1){
+        if(this.size >= n / 2){
             this.resize(n * 2);
         }
-        int hash = key.hashCode();
-        hash = ((hash % n) + n) % n;
-        if(this.data[hash % n] == null)
-            this.data[hash % n] = new Record(key, value);
-        else{
-            int i = hash % n;
-            while(this.data[i] != null){
-                i++;
-            }
-            this.data[i] = new Record(key,value);
-        }
+        int hash = getIndexLinearProbing(key);
+        this.data[hash] = new Record(key, value);
+        this.size++;
     }
 
     private int getIndexLinearProbing(K query){
         int hash = query.hashCode();
-        hash = ((hash % n) + n) % n;
+        hash = fixHash(hash);
         while(this.data[hash % n] != null 
         && !this.data[hash % n].key.equals(query)){
             hash++;
         }
-        if(this.data[hash % n] == null) return -1;
-        return hash % n;
+        return fixHash(hash);
     }
 
     public boolean contains(K query){
         int hash = getIndexLinearProbing(query);
-        return hash == -1;
+        return data[hash] == null;
     }
 
     public V get(K query){
         int hash = getIndexLinearProbing(query);
-        if(hash == -1) return null;
+        if(data[hash] == null) return null;
         else return this.data[hash].value;
     }
 
     public void set(K key, V value){
         int hash = getIndexLinearProbing(key);
-        if(hash == -1) this.put(key, value);
+        if(data[hash] == null) this.put(key, value);
         else this.data[hash].value = value;
     }
 
     public V remove(K key){
         int hash = getIndexLinearProbing(key);
-        if(hash == -1) return null;
+        if(data[hash] == null) return null;
         else{
             V value = this.data[hash].value;
             this.data[hash] = null;
+            this.size--;
             return value;
         }
+        
     }
 
     public String toString(){
@@ -103,6 +115,15 @@ public class HashMap<K extends Comparable<K>,V> {
         map.put(-150, "Dave");
         System.out.println(map);
         System.out.println(map.get(-150));
+        for(int i = 0; i < 1000; i++){
+            map.put(i, Integer.toString(i));
+        }
+        for(int i = 0; i < 1000; i++){
+            map.set(i, Integer.toString(i) + "new");
+        }
+        for(int i = 0; i < 1000; i++){
+            System.out.println(map.remove(i));
+        }
     }
 
 
